08f2d6fb3ebb58007a0fc3b942e641163540fc8d
Eliecer-23 <jottaperez1975@gmail.com>
2026-04-20 20:11:01 -0500

    .

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -9,8 +9,8 @@
         buscar = buscar.trim();
     }
 
-    for (let i = 0; i < nombres.length; i++) {
-        if (nombres[i].toLowerCase() === buscar.toLowerCase()) {
+    for (let i = 0; i < nombre.length; i++) {
+        if (nombre[i].toLowerCase() === buscar.toLowerCase()) {
             encontrado = true;
             break;
         }
@@ -18,10 +18,11 @@
 
     let contenedor = document.getElementById("ejercicio15");
 
+
     if (encontrado) {
         contenedor.innerHTML = 
-        `el nombre <strong>${buscar}</strong> si en la lista.<br>`;
+        `el nombre <strong>${buscar}</strong> SI esta en la lista.<br>`;
         } else {
         contenedor.innerHTML = 
-        ` el nombre <strong>${buscar}</strong> no esta en la lista. <br>`
+        ` el nombre <strong>${buscar}</strong> NO esta en la lista. <br>`
         }
